784745c2494e8eaf489dd63434d1751e90bdfa1c
Alexander Schenkel <aschenkel@kadenpartner.ch>
2022-09-28 10:50:51 +0200

    Umbau zu Reverse-Proxy-URLs

--- frontend/server.js
+++ frontend/server.js
@@ -11,11 +11,28 @@
  */
 const express = require("express");
 const config = require("./config/config.js");
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
 
 const app = express();
 const port = config.server.port;
+const backendServer = 'http://monolith:3000'
 
 /** ------------------- URL-Routen und -Handler konfigurieren -------------------- */
+
+/** -------------- Reverse Proxy für Backend-Routen --------------- */
+app.all("/feedback", function(req, res) {
+  console.log('Reverse-Proxy für Feedback-Form');
+  proxy.web(req, res, {target: backendServer});
+});
+app.all("/api/save-text", function(req, res) {
+  console.log('Reverse-Proxy für API: save-text');
+  proxy.web(req, res, {target: backendServer});
+});
+app.all("/api/get-texts", function(req, res) {
+  console.log('Reverse-Proxy für API: get-texts');
+  proxy.web(req, res, {target: backendServer});
+});
 
 // statische (Frontend)-Site: alle Files unter site/ werden
 // als statische Dateien ausgeliefert:
